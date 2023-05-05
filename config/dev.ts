ac4a5e26de52010009b2bfbec27225a473750e07
Tangjj1996 <awesometangji@gmail.com>
2023-05-05 18:04:56 +0800

    feat: Convert rollup cli to rollup js

--- config/dev.ts
+++ config/dev.ts
@@ -1,4 +1,51 @@
-import { watch } from "rollup";
-import config from "./base";
+import path from "node:path";
+import commonjs from "@rollup/plugin-commonjs";
+import html from "@rollup/plugin-html";
+import terser from "@rollup/plugin-terser";
+import typescript from "@rollup/plugin-typescript";
+import { glob } from "glob";
+import { nodeResolve } from "@rollup/plugin-node-resolve";
+import { defineConfig, watch } from "rollup";
+import { copy } from "./plugin/copy-plugin";
+
+const isProd = process.env.NODE_ENV === "production";
+
+const config = defineConfig([
+  {
+    input: ["./src/background/index.tsx"],
+    output: {
+      dir: "dist/background",
+    },
+    plugins: [
+      nodeResolve(), // find in node_modules
+      commonjs(), // convert to esModule
+      typescript(),
+      isProd && terser(),
+    ],
+  },
+  {
+    input: ["./src/popup/index.tsx"],
+    output: {
+      dir: "dist/popup",
+    },
+    plugins: [
+      nodeResolve(), // find in node_modules
+      commonjs(), // convert to esModule
+      html(),
+      typescript(),
+      isProd && terser(),
+    ],
+  },
+  {
+    input: glob
+      .sync("src/**/*")
+      .filter((file) => !/background|popup/.test(file))
+      .map((file) => path.relative("src", file)),
+    output: {
+      dir: "dist",
+    },
+    plugins: [copy()],
+  },
+]);
 
 watch(config);
--- config/prod.ts
+++ config/prod.ts
@@ -1,4 +1,83 @@
+import path from "node:path";
+import commonjs from "@rollup/plugin-commonjs";
+import html from "@rollup/plugin-html";
+import terser from "@rollup/plugin-terser";
+import typescript from "@rollup/plugin-typescript";
+import consola from "consola";
+import { glob } from "glob";
+import { nodeResolve } from "@rollup/plugin-node-resolve";
 import { rollup } from "rollup";
-import config from "./base";
 
-rollup({});
+import type { InputOptions, OutputOptions } from "rollup";
+
+import { copy } from "./plugin/copy-plugin";
+
+const backgroundInput: InputOptions = {
+  input: "./src/background/index.tsx",
+  plugins: [
+    nodeResolve(), // find in node_modules
+    commonjs(), // convert to esModule
+    typescript(),
+    terser(),
+  ],
+};
+
+const backgroundOutput: OutputOptions = {
+  dir: "dist/background",
+};
+
+const popupInput: InputOptions = {
+  input: "./src/popup/index.tsx",
+  plugins: [
+    nodeResolve(), // find in node_modules
+    commonjs(), // convert to esModule
+    html(),
+    typescript(),
+    terser(),
+  ],
+};
+
+const popupOutput: OutputOptions = {
+  dir: "dist/popup",
+};
+
+const otherInput: InputOptions = {
+  input: glob
+    .sync("src/**/*")
+    .filter((file) => !/background|popup/.test(file))
+    .map((file) => path.relative("src", file)),
+  plugins: [copy()],
+};
+
+const otherOutput: OutputOptions = {
+  dir: "dist",
+};
+
+async function run() {
+  let buildFailed = false;
+  const inputBundle = await Promise.all([
+    rollup(backgroundInput),
+    rollup(popupInput),
+    rollup(otherInput),
+  ]).catch((error) => {
+    buildFailed = true;
+    consola.error(error);
+  });
+  if (inputBundle) {
+    const [backgroundBundle, popupBundle, otherBundle] = inputBundle;
+    await Promise.all([
+      backgroundBundle.write(backgroundOutput),
+      popupBundle.write(popupOutput),
+      otherBundle.write(otherOutput),
+    ]).catch((error) => {
+      buildFailed = true;
+      consola.error(error);
+    });
+    backgroundBundle.close();
+    popupBundle.close();
+    otherBundle.close();
+  }
+  process.exitCode = buildFailed ? 1 : 0;
+}
+
+await run();
