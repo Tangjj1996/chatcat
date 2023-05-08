08811ae6d0e8bb70a230d978dbab4ab3521cdcb7
Tangjj1996 <awesometangji@gmail.com>
2023-05-08 10:30:30 +0800

    feat: Use esbuild mode replace chokidar

---
+++ script/config.dev.ts
@@ -0,0 +1,73 @@
+import esbuild from "esbuild";
+import fs from "fs-extra";
+import consola from "consola";
+import postcssplugin from "esbuild-style-plugin";
+import autoprefixer from "autoprefixer";
+import tailwindcss from "tailwindcss";
+import * as dotenv from "dotenv";
+
+import { OUTPUT_DIR } from "./const";
+
+dotenv.config();
+
+async function removeOldDir() {
+  await fs.remove(OUTPUT_DIR);
+  consola.success("Remove old Direction successfully");
+}
+
+async function runEsbuild() {
+  const ctx = await esbuild.context({
+    entryPoints: ["src/background/index.tsx", "src/popup/index.tsx"],
+    bundle: true,
+    outdir: OUTPUT_DIR,
+    treeShaking: true,
+    minify: true,
+    define: {
+      "proccess.env.ACCESS_TOKEN": process.env.ACCESS_TOKEN!,
+    },
+    loader: {
+      ".png": "dataurl",
+    },
+    plugins: [
+      postcssplugin({
+        postcss: {
+          plugins: [tailwindcss, autoprefixer],
+        },
+      }),
+    ],
+  });
+  await ctx.watch();
+  consola.success("Esbuild bundle successfully");
+}
+
+interface EntryPoint {
+  src: string;
+  dest: string;
+}
+
+async function copyFiles(entryPoints: EntryPoint[]) {
+  await fs.ensureDir(OUTPUT_DIR);
+  await Promise.all(
+    entryPoints.map(async (entryPoint) => {
+      await fs.copy(entryPoint.src, `${OUTPUT_DIR}/${entryPoint.dest}`);
+    })
+  );
+  consola.success("Copy files successfully");
+}
+
+async function build() {
+  consola.start("Let's start, bro...");
+
+  await removeOldDir();
+  await runEsbuild();
+
+  await copyFiles([
+    { src: "src/manifest.json", dest: "manifest.json" },
+    { src: "src/logo.png", dest: "logo.png" },
+    { src: "src/popup/index.html", dest: "popup/index.html" },
+  ]);
+
+  consola.success("All steps success, nice!");
+}
+
+await build();
